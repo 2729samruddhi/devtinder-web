ef0c5b253744ac1c656dcc8ced0d1bd967b8bef1
Samruddhi <patilsamruddhi42@gmail.com>
2025-12-10 19:51:06 +0530

    use daisyui navbar in app.js

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # DevTinder
-- create vite + rect app
+- create vite + rect app    
 - remove unecessary code and create hello world app
 - install tailwind css packages
 - install daisy ui
+- use navbar component in aap.js
--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,36 @@
 function App() {
 
   return (
-    <>
-      <h1 className="text-2xl ">Hello world!!</h1>
-      <button
-  class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-  Button
-</button>
+    < >
+      <div className="navbar bg-base-300 shadow-sm">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">👩🏻‍❤️‍🧑🏻 DevTinder</a>
+  </div>
+  <div className="flex gap-2 mx-4">
+    {/* <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" /> */}
+    <div className="dropdown dropdown-end">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
+      <ul
+        tabIndex="-1"
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
     </>
   )
 }
